2568fb0adab0a5cdcb579f5452d4583651c1b029
Adam <adam@mradam.us>
2020-12-19 13:35:28 -0600

    create add todo function and styling todo page

--- app.js
+++ app.js
@@ -2,3 +2,35 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+
+function addTodo(e) {
+  //Prevent form from submitting
+  e.preventDefault();
+  //Create Todo div
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  //Create li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  //check mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+  //check trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('trash-btn');
+  todoDiv.appendChild(trashButton);
+  //Append to list
+  todoList.appendChild(todoDiv);
+  //Clearn input
+  todoInput.value = '';
+}
